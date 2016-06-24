1045b22a83a7dc691d6e8706bb519edbfcdff104
wormsleep <wormsleep@gmail.com>
2016-06-24 18:14:03 +0800

    增强 - 修改 c3p0 配置支持最小化配置

--- src/main/java/zw/wormsleep/tools/etl/database/ConnectionPool.java
+++ src/main/java/zw/wormsleep/tools/etl/database/ConnectionPool.java
@@ -18,14 +18,26 @@ public class ConnectionPool {
     static final String JDBC_URL = "jdbcUrl";
     static final String USER = "user";
     static final String PASSWORD = "password";
-    static final String MIN_POOL_SIZE = "minPoolSize";
-    static final String MAX_POOL_SIZE = "maxPoolSize";
+
+    // 当连接池满了 c3p0 一次性获取的连接数（默认值：3）
     static final String ACQUIRE_INCREMENT = "acquireIncrement";
-    static final String INITIAL_POOL_SIZE = "initialPoolSize";
-    static final String IDLE_CONNECTION_TEST_PERIOD = "idleConnectionTestPeriod";
-    static final String CHECKOUT_TIMEOUT = "checkoutTimeout";
+    // 定义当从数据库获取新连接时 c3p0 尝试的次数（默认值：30）
     static final String ACQUIRE_RETRY_ATTEMPTS = "acquireRetryAttempts";
-    static final String TEST_CONNECTION_ON_CHECKOUT = "testConnectionOnCheckout";
+    // c3p0 获取间隔的时间（默认值：1000 毫秒）
+    static final String ACQUIRE_RETRY_DELAY = "acquireRetryDelay";
+    // c3p0 是异步操作的缓慢的 JDBC 操作通过帮助进程完成。扩展这些操作可以有效的提升性能，通过多线程实现多个操作同时被执行。（默认值：3）
+    static final String NUM_HELPER_THREADS = "numHelperThreads";
+    // 连接池初始化时创建的连接数（默认值：3）
+    static final String INITIAL_POOL_SIZE = "initialPoolSize";
+    // 连接池保持的最小连接数（默认值：3）
+    static final String MIN_POOL_SIZE = "minPoolSize";
+    // 连接池拥有的最大连接数（默认值：15）
+    static final String MAX_POOL_SIZE = "maxPoolSize";
+    // 间隔多少秒检查所有连接池中的空闲连接（默认值：0 表示不检查）
+    static final String IDLE_CONNECTION_TEST_PERIOD = "idleConnectionTestPeriod";
+    // 当连接池用完时客户端等待获取新连接的时间，超时后将抛出 SQLException （默认值：0 毫秒。表示无限等待）
+    static final String CHECKOUT_TIMEOUT = "checkoutTimeout";
+    // 测试连接的语句，若不设置则通过 getTables() 获取 MetaData 其速度将远慢于设定该语句的情况。（默认值：null）（推荐设置）
     static final String PREFERRED_TEST_QUERY = "preferredTestQuery";
 
     private static Map<String, ComboPooledDataSource> pool = new HashMap<String, ComboPooledDataSource>();
@@ -37,28 +49,33 @@ public class ConnectionPool {
             throws PropertyVetoException {
         ComboPooledDataSource cpds = new ComboPooledDataSource();
 
-        // 驱动器
+        // 必须配置的项目
         cpds.setDriverClass(poolConfig.get(DRIVER_CLASS));
-        // 数据库url
         cpds.setJdbcUrl(poolConfig.get(JDBC_URL));
-        // 用户名
         cpds.setUser(poolConfig.get(USER));
-        // 密码
         cpds.setPassword(poolConfig.get(PASSWORD));
-        // 初始化连接池大小
-        cpds.setInitialPoolSize(Integer.valueOf(poolConfig.get(INITIAL_POOL_SIZE)));
-        // 最少连接数
-        cpds.setMinPoolSize(Integer.valueOf(poolConfig.get(MIN_POOL_SIZE)));
-        // 最大连接数
-        cpds.setMaxPoolSize(Integer.valueOf(poolConfig.get(MAX_POOL_SIZE)));
-        // 连接数的增量
-        cpds.setAcquireIncrement(Integer.valueOf(poolConfig.get(ACQUIRE_INCREMENT)));
-        // 测连接有效的时间间隔
-        cpds.setIdleConnectionTestPeriod(Integer.valueOf(poolConfig.get(IDLE_CONNECTION_TEST_PERIOD)));
-        cpds.setCheckoutTimeout(Integer.valueOf(poolConfig.get(CHECKOUT_TIMEOUT)));
-        cpds.setAcquireRetryAttempts(Integer.valueOf(poolConfig.get(ACQUIRE_RETRY_ATTEMPTS)));
-        cpds.setTestConnectionOnCheckout(poolConfig.get(TEST_CONNECTION_ON_CHECKOUT).equalsIgnoreCase("true") ? true : false);
-        cpds.setPreferredTestQuery(poolConfig.get(PREFERRED_TEST_QUERY));
+
+        // 可选（或者拥有默认值的项目）
+        if (poolConfig.get(INITIAL_POOL_SIZE) != null)
+            cpds.setInitialPoolSize(Integer.valueOf(poolConfig.get(INITIAL_POOL_SIZE)));
+        if (poolConfig.get(MIN_POOL_SIZE) != null)
+            cpds.setMinPoolSize(Integer.valueOf(poolConfig.get(MIN_POOL_SIZE)));
+        if (poolConfig.get(MAX_POOL_SIZE) != null)
+            cpds.setMaxPoolSize(Integer.valueOf(poolConfig.get(MAX_POOL_SIZE)));
+        if (poolConfig.get(ACQUIRE_INCREMENT) != null)
+            cpds.setAcquireIncrement(Integer.valueOf(poolConfig.get(ACQUIRE_INCREMENT)));
+        if (poolConfig.get(ACQUIRE_RETRY_ATTEMPTS) != null)
+            cpds.setAcquireRetryAttempts(Integer.valueOf(poolConfig.get(ACQUIRE_RETRY_ATTEMPTS)));
+        if (poolConfig.get(ACQUIRE_RETRY_DELAY) != null)
+            cpds.setAcquireRetryAttempts(Integer.valueOf(poolConfig.get(ACQUIRE_RETRY_DELAY)));
+        if (poolConfig.get(IDLE_CONNECTION_TEST_PERIOD) != null)
+            cpds.setIdleConnectionTestPeriod(Integer.valueOf(poolConfig.get(IDLE_CONNECTION_TEST_PERIOD)));
+        if (poolConfig.get(CHECKOUT_TIMEOUT) != null)
+            cpds.setCheckoutTimeout(Integer.valueOf(poolConfig.get(CHECKOUT_TIMEOUT)));
+        if (poolConfig.get(NUM_HELPER_THREADS) != null)
+            cpds.setCheckoutTimeout(Integer.valueOf(poolConfig.get(NUM_HELPER_THREADS)));
+        if (poolConfig.get(PREFERRED_TEST_QUERY) != null)
+            cpds.setPreferredTestQuery(poolConfig.get(PREFERRED_TEST_QUERY));
 
         return cpds;
 
@@ -67,19 +84,23 @@ public class ConnectionPool {
     public static Connection getConnection(String database,
                                            Map<String, String> poolConfig) throws PropertyVetoException, SQLException {
 
-        ComboPooledDataSource cpds = (ComboPooledDataSource) pool.get(database);
+        ComboPooledDataSource cpds = pool.get(database);
 
         if (cpds == null) {
             cpds = create(poolConfig);
             pool.put(database, cpds);
         }
 
+        logger.info("@@@ 数据库节点：{} 从连接池获取连接成功！\n" +
+                "连接池状态 - 总连接数：{} - 使用：{} - 空闲：{}",
+                database, cpds.getNumConnections(), cpds.getNumBusyConnections(), cpds.getNumIdleConnections());
+
         return cpds.getConnection();
 
     }
 
     public static void release(String database) {
-        ComboPooledDataSource cpds = (ComboPooledDataSource) pool.get(database);
+        ComboPooledDataSource cpds = pool.get(database);
         if (cpds != null) {
             cpds.close();
         }
